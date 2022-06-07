de43a0c1c7bdd1965449495a08302649abc7884c
Maciej Matula <maciek.matula@gmail.com>
2022-06-06 21:37:35 -0500

    commit tree f15b3f9bab9b3f7e9a6af8dab3bd552547e690aa

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const inputField = document.querySelector('#input')
+  inputField.addEventListener('click', clickAlert)
+}
+
+function clickAlert(){
+  alert("AH! YOU GOT ME!")
 }
